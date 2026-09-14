ffcbfda2f38556fdfe4d8b5afec3c76461416f68
Courey J <coureyfrance@gmail.com>
2026-09-14 15:34:39 -0400

    Error handling

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 #R GLAB 385.3.1
 
 #Add contacts
+    # define a func called "add_contact"
+    # get name, phone from the user
+
+def add_contact():
+    print('Add Contact')
+    name = input("Enter Name: ")
+    phone = input("Enter phone number: ")
+
+    try:
+        if len(name) == 0:
+            raise ValueError("User must input a valid name.")
+
+        if len(phone) == 0:
+                    raise ValueError("User must input a valid number.")
+
+        with open('data/contacts.txt', mode='a') as f:
+            f.write(f'{name}: {phone}\n')
+
+        print(f' ✅ {name} has been added to your contacts!')
+
+    except ValueError as e:
+        print(f'Value Error: {e}')
+
+    except Exception as e:
+            print(f'❌ Error: {e}')
+
 #View contacts
-#Quit out of applications
+#Quit out of applications ✅
 
 # Declare a function called "main" it should contain a while True: loop
 def main ():
     while True:
         # logic
-        print( '==== ☎️ Contact List Application ☎️ ====')
+        print( '\n==== ☎️ Contact List Application ☎️ ====')
         print('1.   Add Contact')
         print('2.   View Contact')   
         print('3.   Quit')
@@ -17,7 +43,7 @@ def main ():
 
 # Control flow 'if' statement to ctrl actions program takes
         if choice == '1':
-            print('Add Contact')
+            add_contact()
         elif choice == '2':
             print('View Contacts')
         elif choice == '3':
@@ -25,3 +51,7 @@ def main ():
             break
         else: 
             print('❌ Invalid choice ❌ Please try again.')
+
+# call the main func
+if __name__ == '__main__':
+    main()
